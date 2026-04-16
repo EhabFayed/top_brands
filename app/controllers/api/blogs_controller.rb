5fa2706ea097e3432efa260df21d2b6f5e152861
ehab fayed <efayed@damlag.com>
2026-04-16 04:38:24 +0200

    missed alter for image on content of the blogs

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -73,7 +73,7 @@ module Api
         is_published: blog.is_published,
         category: blog.category,
         photos: blog.blog_photos.map { |p| { id: p.id, alt_ar: p.alt_ar, alt_en: p.alt_en, photo_url: p.cached_photo_url } },
-        contents: blog.contents.map { |c| { id: c.id, key: c.key, value: c.value, photos: c.content_photos.map {|cp| {url: cp.cached_photo_url} } } }
+        contents: blog.contents.map { |c| { id: c.id, key: c.key, value: c.value, photos: c.content_photos.map {|cp| {url: cp.cached_photo_url, alt_ar: cp.alt_ar, alt_en: cp.alt_en} } } }
       }
     end
   end
